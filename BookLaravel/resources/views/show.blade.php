76e9bfec11ead707554955b83d3b9326811dba8f
unknown <mortujaaiub@gmail.com>
2023-04-17 11:28:43 +0600

    test

---
+++ BookLaravel/resources/views/show.blade.php
@@ -0,0 +1,47 @@
+@extends('products.layout')
+  
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2> Show Product</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('products.index') }}"> Back</a>
+            </div>
+        </div>
+    </div>
+   
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Name:</strong>
+                {{ $product->name }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>P_name:</strong>
+                {{ $product->p_name }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>age:</strong>
+                {{ $product->age }}
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>P_date:</strong>
+                {{ $product->p_date }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>P_name:</strong>
+                {{ $product->age }}
+            </div>
+        </div>
+        </div>
+    </div>
+@endsection
